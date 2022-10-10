b0c17695a3776a7b07e1a9069f3afd200a914b18
Amesika <am3sika@gmail.com>
2022-10-10 06:16:49 +0200

    Test Jobs migration

--- jenkins/vagrant/install_jenkins.sh
+++ jenkins/vagrant/install_jenkins.sh
@@ -2,7 +2,7 @@
 
 # Install java
 sudo apt update
-sudo apt install openjdk-11-jre
+sudo apt install openjdk-11-jre -y
 java -version
 
 sudo nano /etc/environment
@@ -12,11 +12,8 @@ JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
 sudo apt-get install maven -y
 
 # Install Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
 
